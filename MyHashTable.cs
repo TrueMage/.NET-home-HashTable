57335ef5bed0de700e7b6a619f033df8a23e4b57
Andrii Hromanchuk <73118147+TrueMage@users.noreply.github.com>
2023-10-22 21:44:59 +0200

    Add project files.

---
+++ MyHashTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace home_HashTable
+{
+    internal class MyHashTable
+    { // dictionary, возможно, в разы облегчили написание, но вы их не запрещали!
+        private Dictionary<int, Dictionary<string, int>> _hashtable = new Dictionary<int, Dictionary<string, int>>{};
+
+        public void Add(string key, int value)
+        {
+            int hashKey = HashFunction(key);
+
+            if (!_hashtable.ContainsKey(hashKey))
+            {
+                _hashtable.Add(hashKey, new Dictionary<string, int>());
+                _hashtable[hashKey].Add(key, value);
+            }
+            else _hashtable[hashKey].Add(key, value);
+        }
+
+        public void Remove(string key)
+        {
+            int hashKey = HashFunction(key);
+
+            if (!_hashtable.ContainsKey(hashKey)) throw new Exception("There is no contact " + key + "in the phonebook");
+            else if (!_hashtable[hashKey].ContainsKey(key))
+                throw new Exception("There is no " + key + " in the phonebook");
+            else _hashtable[hashKey].Remove(key);
+        }
+
+        public int Find(string key)
+        {
+            int hashKey = HashFunction(key);
+
+            if (!_hashtable.ContainsKey(hashKey)) return -1;
+            else if (!_hashtable[hashKey].ContainsKey(key)) return -1;
+            else return _hashtable[hashKey][key];
+        }
+
+        private int HashFunction(string key)
+        {
+            // https://learn.microsoft.com/en-us/troubleshoot/developer/visualstudio/csharp/language-compilers/compute-hash-values
+            byte[] tmpSource = ASCIIEncoding.ASCII.GetBytes(key);
+            byte[] tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+
+            int hashKey = BitConverter.ToInt32(tmpHash);
+            return hashKey;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace home_HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHashTable phoneBook = new MyHashTable();
+
+            phoneBook.Add("John White", 34534);
+
+            Console.WriteLine(phoneBook.Find("John White"));
+            phoneBook.Remove("John White");
+
+            try
+            {
+                phoneBook.Remove("John White");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
